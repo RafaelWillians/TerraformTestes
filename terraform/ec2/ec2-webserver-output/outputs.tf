7ffbbf6e1d03c342a6f13a4da8bfef5fc8dd9c78
Rafael Silva Willians <rafael.willians@outlook.com>
2026-04-19 15:42:39 +0000

    Adicionado coalesce para ip nao disponivel

--- terraform/ec2/ec2-webserver-output/outputs.tf
+++ terraform/ec2/ec2-webserver-output/outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-    value = aws_instance.webserver-exemplo.public_ip
+    value = coalesce(aws_instance.webserver-exemplo.public_ip, "IP nao disponivel")
     description = "Endereco publico do servidor web"
 }
