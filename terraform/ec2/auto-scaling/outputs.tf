050ba9c37faa6f474b79ca647753ab81cb584909
Rafael Silva Willians <rafael.willians@outlook.com>
2026-04-20 20:52:00 +0000

    Adicionado loadbalancer e mudado output para apontar dns

--- terraform/ec2/auto-scaling/outputs.tf
+++ terraform/ec2/auto-scaling/outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-    value = coalesce(aws_instance.webserver-exemplo.public_ip, "IP nao disponivel")
-    description = "Endereco publico do servidor web"
+    value = coalesce(aws_lb.exemplo.dns_name, "DNS do load balancer nao disponivel")
+    description = "Endereco DNS do loadbalancer"
 }
