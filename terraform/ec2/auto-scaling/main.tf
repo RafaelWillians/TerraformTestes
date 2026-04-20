3b99421ca59e6b7dc6a2e77e3059222c73d3df7d
Rafael Silva Willians <rafael.willians@outlook.com>
2026-04-20 18:54:26 +0000

    Adicionado data source, asg e alb

--- terraform/ec2/auto-scaling/main.tf
+++ terraform/ec2/auto-scaling/main.tf
@@ -26,3 +26,39 @@ resource "aws_security_group" "instance" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_launch_configuration" "exemplo" {
+    image_id = "ami-1234"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, world" > index.html
+        nohub busybox httpd -f -p ${var.server_port} &
+        EOF
+
+    lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "exemplo" {
+    launch_configuration = aws_launch_configuration.exemplo.name
+    vpc_zone_identifier = data.aws_subnets.default.ids
+
+    min_size = 3
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-exemplo"
+        propagate_at_launch = true      
+    }
+}
+
+resource "aws_lb" "exemplo" {
+    name = "terraform-asg-exemplo"
+    load_balancer_type = "application"
+    subnets = data.aws_subnets.default.ids
+}
